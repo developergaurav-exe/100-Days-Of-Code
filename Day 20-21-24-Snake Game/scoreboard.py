f05e8844dd488d4ef198f9cf43e738ba4ba4c763
Harsh Gaurav <agentplatypusperry7@gmail.com>
2022-08-23 20:03:53 +0530

    Day 24/ Add High_Score Save Functionality in Snake Game/ OOPs/ Turtle Module

--- Day 20-21-24-Snake Game/scoreboard.py
+++ Day 20-21-24-Snake Game/scoreboard.py
@@ -1,3 +1,4 @@
+from operator import mod
 from turtle import Turtle
 
 ALIGN = 'center'
@@ -6,9 +7,10 @@ FONT = ('Courier', 24, 'normal')
 class Scoreboard(Turtle):
     def __init__(self) -> None:
         super().__init__()
-
         self.score = 0
-        self.high_score = 0
+        with open('saved_data.txt') as f:
+            self.high_score = int(f.read()) # have to convert to int to make it calcutable further
+
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -23,6 +25,9 @@ class Scoreboard(Turtle):
     def reset_scoreboard(self):
         if self.high_score < self.score:
             self.high_score = self.score
+            with open('saved_data.txt', mode='w') as f:
+                f.write(f"{self.high_score}") # write function can only write string
+
         self.score = 0
         self.update_scoreboard()
         
