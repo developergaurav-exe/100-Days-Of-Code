850bcdcb6b4baf3a2ee49edccd4d7971292f767d
Harsh Gaurav <agentplatypusperry7@gmail.com>
2022-08-23 18:30:52 +0530

    Day 24/ Modify Snake Game/ OOPs/ Turtle Module

--- Day 20-21-24-Snake Game/scoreboard.py
+++ Day 20-21-24-Snake Game/scoreboard.py
@@ -8,7 +8,7 @@ class Scoreboard(Turtle):
         super().__init__()
 
         self.score = 0
-
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -17,15 +17,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align= ALIGN, font= FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align= ALIGN, font= FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("Game Over.", align= ALIGN, font= FONT)
+    def reset_scoreboard(self):
+        if self.high_score < self.score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
+        
+    # def game_over(self):
+    #     self.goto(0,0)
+    #     self.write("Game Over.", align= ALIGN, font= FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
     
         
